3825ded424015b1b7abe5f44ac4632e1d5e19372
esuandrius <andrius.adom@gmail.com>
2023-01-10 12:58:22 +0200

    write comment testas in App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Footer from "./components/footer/Footer";
 import { withTranslation } from "react-i18next";
 import LanguageSwitcher from "./languageSwitcher";
 
+// testas
+
 const App = () => {
   return (
     <>
